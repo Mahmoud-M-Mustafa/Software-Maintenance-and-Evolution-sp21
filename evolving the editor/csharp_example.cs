9b294b18f2c4139ef600a1cd6efd518ee981fdcb
Mahmoud Mustafa <mahmoud.m.mustafa@hotmail.com>
2021-05-30 18:48:17 +0200

    first commit

---
+++ evolving the editor/csharp_example.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+
+//  Single Line comment
+
+/*   
+   block commnet
+   
+*/
+
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
